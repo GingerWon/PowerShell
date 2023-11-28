f7a7cc288d22c91ed6f64f55ba4668a7e0f64c46
Justin <jmginger92@gmail.com>
2023-11-28 11:09:55 -0700

    Add files via upload
    These won't work without some minor customization, as I've generalized sensitive information.

---
+++ PasswordExpirationNotification.ps1
@@ -0,0 +1,64 @@
+﻿Import-Module ActiveDirectory
+function Send-Mail ($to)      {
+
+$cred = get-credential -credential "#email@domain.com#"
+$From = "#email@domain.com#"
+$To = "#email@domain.com#"
+$SMTPServer = "smtp.office365.com"
+$SMTPPort = "587"
+$subject = "Password Expiration Notification"
+$SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer,$SMTPPort) 
+$SMTPClient.EnableSsl = $true
+$message = "Hello, 
+
+This is a reminder that your password is set to expire in three days or less. Because we often see authentication/lock out issues when we change passwords, we ask that you please change it today rather than over the weekend.  This way we are better able to assist if an issue arises.
+                                                                                                                                                                       
+
+To change your password - US On-site Employees
+
+1.  Press CTRL + ALT + DEL
+2.  Select 'Change Your Password...'
+3.  Enter your existing password and then your new desired password
+4.  Update any mobile device that uses COMPANY email with your new password
+
+
+
+To change your password - Off-site or Traveling Employees
+
+1.  Log into https://citrix.COMPANY.com
+2.  Select the Accessories folder
+3.  Select COMPANY Password Tool
+4.  Follow the prompts to change your password
+5.  Update any mobile device that uses COMPANY email with your new password
+
+Password Complexity Rules:
+- must be at least 6 characters
+- must contain three of the following: uppercase character, lowercase character, numerical value, special character
+- cannot contain parts of your first or last name
+
+Clearing your Credential manager:
+Your local computer saves your passwords in the Credential Manager. It is important to clear out your old password, so it does not lock you out of your account. Please see the attached instructions on how to clear your credential manager. 
+
+
+
+Please contact Helpdesk@wirecoworldgroup.com if you need any assistance resetting your password."
+$message.isbodyhtml = $true 
+$SMTPClient.Credentials = new-object system.net.networkcredential($cred.GetNetworkCredential().UserName,$cred.GetNetworkCredential().Password)
+$SMTPClient.Send($message)}
+
+
+$Users = get-aduser -filter {(Enabled -eq $true) -and (PasswordNeverExpires -eq $false)} -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress |
+        where { $_.passwordexpired -eq $false }  | 
+            select passwordlastset, EmailAddress | 
+               
+        foreach     {
+            New-Object -TypeName psobject @{
+            Expires = ((get-date) - $_.PasswordLastSet).totaldays
+            Email = $_.emailaddress
+                    } }
+
+$targets =   $Users | Where-Object{$_.expires -gt 176 -and $_.expires -lt 180} | foreach send-
+   
+
+
+
---
+++ TerminationDataMigrationV4.ps1
@@ -0,0 +1,99 @@
+﻿<# 
+1. Requests tech to select username from C:/Users folder. Selects relevant data, reports number of files found.
+2. Requests termination date and the length of Hold Term.
+3. Creates a "LaptopSN-Username-Date" folder and corresponding subfolders on H drive.
+4. Copies data to the corresponding folder on the H drive. (Does not move empty folders.)
+5. Sends an email to notify you when the script has completed.
+#>
+
+Write-host  -ForegroundColor red "This script moves a user's data when they have been terminated.  It intentionally excludes any .exe and .msi files per IT Policy, so double check that you don't need them. `n"
+
+$UserProfiles = Get-ChildItem -Path C:\users\ |
+Where-Object {
+                ($_.name -notlike "Administrator*") -and
+                ($_.Name -notlike "COMPANYNAME*") -and
+                ($_.name -notlike "public*") -and
+                ($_.name -notlike "*-a*")
+             }
+
+$users = @($UserProfiles) 
+$n = 0
+$menutext = ""
+foreach ($user in $Users)   {
+$menutext = $menutext + "$($n+1): Type '$($n+1)' for $($users[$n]) and press Enter. `n"
+$n =+ 1
+                            }
+
+           
+[string]$Title = 'Select a User'
+Write-Host "================ $Title ================"
+Write-Host $menutext  
+        
+
+
+$input = Read-Host "Please make a selection"
+$input2 = switch ($input)
+    {
+        '1' {$($users[0])}
+        '2' {$($users[1])}
+        '3' {$($users[2])}
+        '4' {$($users[3])}
+        '5' {$($users[4])}
+        '6' {$($users[5])}
+        '7' {$($users[6])}
+        '8' {$($users[7])}
+        '9' {$($users[8])}
+        '10' {$($users[9])}
+        default {"Please select a valid number representing a username."}
+    }
+
+if ($input2 = 'default') {Write-Host $input2 ; break} else {
+
+       
+
+#date-pickers exist for $termdate, but are complicated
+$TermDate = Read-Host -Prompt "Refer to the MACD and type the termination date in MM-DD-YY format"
+Write-Host "Press 1 for 30-day hold, or 2 for 1-year hold."
+$HoldLength = Read-Host -Prompt "Please make a selection."
+$HoldLength2 = switch ($HoldLength)
+                        {
+                            '1' {"30-Days"}
+                            '2' {"1-Year"}
+                        }
+
+
+$Bios = Get-WmiObject win32_bios
+$SN = $bios.serialnumber
+$sw = [Diagnostics.Stopwatch]::StartNew()
+
+$Desktop = Get-ChildItem -Path C:\Users\$input2\Desktop -Exclude "*.exe", "*.msi" 
+$Downloads = Get-ChildItem -Path C:\Users\$input2\Downloads -Exclude "*.exe", "*.msi"
+$Docs = Get-ChildItem -Path C:\Users\$input2\Documents -Exclude "*.exe", "*.msi"
+#$Music = Get-ChildItem -Path C:\Users\$input2\Music -Exclude "*.exe", "*.msi"
+$Pictures = Get-ChildItem -Path C:\Users\$input2\Pictures -Exclude "*.exe", "*.msi"
+$Videos = Get-ChildItem -Path C:\Users\$input2\Videos -Exclude "*.exe", "*.msi"
+
+Write-Host -ForegroundColor Cyan "There are $($desktop.count) Desktop items, $($Downloads.count) Downloads, $($docs.count) Documents, $($music.count) in Music, $($Pictures.count) Pictures and $($Videos.count) Videos to be moved."
+
+$target2 = "$sn-$input2-$TermDate"
+New-Item -Path \\txalfs02\users\$input2 -ItemType Directory -Name "~Hold for $HoldLength2 from $TermDate"
+New-Item -Path \\txalfs02\users\$input2 -ItemType Directory -Name $target2
+
+if ($data1 -gt 0) {New-Item -Path \\txalfs02\users\$input2\$target2 -ItemType Directory -Name Desktop}
+if ($data2 -gt 0) {New-Item -Path \\txalfs02\users\$input2\$target2 -ItemType Directory -Name Downloads}
+if ($data3 -gt 0) {New-Item -Path \\txalfs02\users\$input2\$target2 -ItemType Directory -Name Docs}
+if ($data4 -gt 0) {New-Item -Path \\txalfs02\users\$input2\$target2 -ItemType Directory -Name Music}
+if ($data5 -gt 0) {New-Item -Path \\txalfs02\users\$input2\$target2 -ItemType Directory -name Pictures}
+if ($data6 -gt 0) {New-Item -Path \\txalfs02\users\$input2\$target2 -ItemType Directory -Name Videos}
+
+<# Should make this a function, or simplify the  data into one variable. #>
+$Desktop | ForEach-Object {Copy-Item -Recurse $_.FullName -Destination \\txalfs02.wrca.com\users\$input2\$target2\Desktop}
+$Downloads | ForEach-Object {Copy-Item -Recurse $_.FullName -Destination \\txalfs02.wrca.com\users\$input2\$target2\Downloads}
+$Docs | ForEach-Object {Copy-Item -Recurse $_.FullName -Destination \\txalfs02.wrca.com\users\$input2\$target2\Docs}
+#$Music | ForEach-Object {Copy-Item -Recurse $_.FullName -Destination \\txalfs02.wrca.com\users\$input2\$target2\Music}
+$Pictures | ForEach-Object {Copy-Item -Recurse $_.FullName -Destination \\txalfs02.wrca.com\users\$input2\$target2\Pictures}
+$Videos | ForEach-Object {Copy-Item -Recurse $_.FullName -Destination \\txalfs02.wrca.com\users\$input2\$target2\Videos}
+
+$sw.Stop()
+Write-Host "All specified data has been moved to $($input2)'s H Drive, and it took $($sw.elapsed) to complete."
+Read-Host -Prompt "Press Enter to continue."}
